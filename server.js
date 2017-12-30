0572284ba5f1b3a5f53023389c51fed0b3a86a24
jamestroutman44 <james.troutman44@gmail.com>
2017-12-29 18:04:09 -0800

    attempted fix on createComment

--- server.js
+++ server.js
@@ -259,19 +259,19 @@ function createComment(url, request) {
   const requestComment = request.body && request.body.comment;
   const response = {};
 
-  if (requestComment && requestComment.body && requestComment.username && requestComment.articleId && database.users[requestComment.username]) {
+  if (requestComment && requestComment.body && requestComment.username && requestComment.articleId && database.users[requestComment.username] && database.articles[requestComment.username]) {
     const comment = {
       id: database.nextCommentId++,
       body: requestComment.body,
       username: requestComment.username,
-      articleId: [],
+      articleId: requestComment.articleId,
       upvotedBy: [],
       downvotedBy: []
     };
 
     database.comments[comment.id] = comment;
     database.users[comment.username].commentIds.push(comment.id);
-
+    database.articles[comment.articleId].commentIds.push(comment.id);
     response.body = {comment: comment};
     response.status = 201;
   } else {
